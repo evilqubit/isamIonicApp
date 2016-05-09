9fe6a2bd55d5f6d119688f57d63aa2661c366312
Jad Salhani <salhanijad@gmail.com>
2016-05-09 13:25:03 +0300

    fixes on sidebar

--- app/app.ts
+++ app/app.ts
@@ -7,49 +7,47 @@ import {LandingPage} from './pages/landing/landing';
 import {CategoriesListPage} from './pages/categories-list/categories-list';
 import {CategoryNewsPage} from "./pages/category-news/category-news";
 import {LookupGradesPage} from "./pages/lookup-grades/lookup-grades";
+import {SearchPage} from './pages/search/search';
 
 @App({
-    templateUrl: 'build/app.html',
-    config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+  templateUrl: 'build/app.html',
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 class MyApp {
-    public rootPage: any = CategoriesListPage;
-    public pages: Array<{ title: string, component: any }>;
-    public categoriesPages: Array<{ title: string, component: any, categoryId: string }>;
+  public rootPage: any = CategoriesListPage;
+  public pages: Array<{ title: string, component: any }>;
+  public categoriesPages: Array<{ title: string, component: any, categoryId: string }>;
 
-    constructor(private app: IonicApp, private platform: Platform) {
-        this.initializeApp();
+  constructor(private app: IonicApp, private platform: Platform) {
+    this.initializeApp();
 
-        // used for an example of ngFor and navigation
-        this.pages = [
-            { title: 'Home', component: LandingPage },
-            { title: 'All News', component: CategoriesListPage },
-            { title: 'Find Grades', component: LookupGradesPage }
-        ];
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Search', component: SearchPage },
+      { title: 'Home', component: CategoriesListPage },
+      { title: 'Find Grades', component: LookupGradesPage }
+    ];
 
-        // this.categoriesPages = [
-        //     { title: 'Universities', component: CategoryNewsPage, categoryId: "C95NB1grX0" },
-        //     { title: 'News', component: CategoryNewsPage, categoryId: "iXO8tgptpL" },
-        //     { title: 'Science', component: CategoryNewsPage, categoryId: "KT5uqdDFQL" }
-        // ];
+    // this.categoriesPages = [
+    //     { title: 'Universities', component: CategoryNewsPage, categoryId: "C95NB1grX0" },
+    //     { title: 'News', component: CategoryNewsPage, categoryId: "iXO8tgptpL" },
+    //     { title: 'Science', component: CategoryNewsPage, categoryId: "KT5uqdDFQL" }
+    // ];
 
-    }
+  }
 
-    initializeApp() {
-        this.platform.ready().then(() => {
-            // Okay, so the platform is ready and our plugins are available.
-            // Here you can do any higher level native things you might need.
-            StatusBar.styleDefault();
-        });
-    }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
 
-    openPage(page) {
-        // Reset the content nav to have just this page
-        // we wouldn't want the back button to show in this scenario
-        let nav = this.app.getComponent('nav');
-        nav.push(page.component, {
-            categoryName: page.title,
-            categoryObjectId: page.categoryId
-        });
-    }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    let nav = this.app.getComponent('nav');
+    nav.setRoot(page.component);
+  }
 }
