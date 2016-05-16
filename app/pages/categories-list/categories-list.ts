ff7afe2ae70bf9b9a2ea2d73db4556e30815649d
Jad Salhani <salhanijad@gmail.com>
2016-05-16 09:15:22 +0300

    Fixes slider to autoplay + adds search icon to navbar + shows proper categoryName based on language

--- app/pages/categories-list/categories-list.ts
+++ app/pages/categories-list/categories-list.ts
@@ -8,19 +8,25 @@ import {NewsDetailsPage} from "../news-details/news-details";
 import {PartnerDetailsPage} from "../partner-details/partner-details";
 import {LookupGradesPage} from "../lookup-grades/lookup-grades";
 import { UserPreference } from '../../providers/user-preference/user-preference';
+import { SearchPage } from '../search/search';
 
 @Page({
   templateUrl: 'build/pages/categories-list/categories-list.page.html'
 })
 export class CategoriesListPage {
+  //Pages used for navigation
   public categoriesPage = CategoriesListPage;
   public categoryNewsPage = CategoryNewsPage;
   public newsDetailsPage = NewsDetailsPage;
+  public gradePage = LookupGradesPage;
+  public searchPage = SearchPage;
+  //Sliders Options
+  public sliderOptions: Object;
+  public adSlidesOptions: Object;
+  //Sliders content
   public adSlides;
   public response;
-  public sliderOptions: Object;
   public partners;
-  public gradePage = LookupGradesPage;
 
   constructor(private _http: Http, private _nav: NavController, private _userPref: UserPreference) {
     this.sliderOptions = {
@@ -28,10 +34,15 @@ export class CategoriesListPage {
       freeMode: true, //	boolean	false	If true then slides will not have fixed positions
       // freeModeMomentum: true //	boolean	true	If true, then slide will keep moving for a while after you release it
     }
+
+    this.adSlidesOptions = {
+      autoplay: 10000,
+      loop: false
+    };
   }
 
   onPageLoaded() {
-    this.getCategories();
+    // this.getCategories();
     this.getSlideAds();
     this.getPartners();
   }
@@ -82,21 +93,23 @@ export class CategoriesListPage {
     let requestHeaders = new Headers();
     requestHeaders.append("X-Parse-Application-Id", "MHY6vxyEIi4SiBZthoSjRib3WLloBwYz9nVXcsou");
     requestHeaders.append("X-Parse-REST-API-Key", "M33K2sDFgY0yT3IniowcLnlKuPqxUgSB6qEmYwmx");
-// where={"langId": ${this._userPref.getSelectedLanguage().id}}&
+    // where={"langId": ${this._userPref.getSelectedLanguage().id}}&
     this._http.get(`https://api.parse.com/1/classes/Category?include=subcat,subcat.subsubcat`, {
-        headers: requestHeaders
-      }).map(res => res.json())
+      headers: requestHeaders
+    }).map(res => res.json())
       .subscribe((data) => {
-        for (let i = 0; i < data.results.length; i++) {
-          if (data.results[i].subcat) {
-            for (let j = 0; j < data.results[i].subcat.length; j++) {
-              if (data.results[i].subcat[j] === null) {
-                data.results[i].subcat.splice(j, 1);
-              }
-            }
-          }
-        }
+        // for (let i = 0; i < data.results.length; i++) {
+        //   if (data.results[i].subcat) {
+        //     for (let j = 0; j < data.results[i].subcat.length; j++) {
+        //       if (data.results[i].subcat[j] === null) {
+        //         data.results[i].subcat.splice(j, 1);
+        //       }
+        //     }
+        //   }
+        // }
+        this.setNameByLanguage(data);
         this.response = data.results;
+
       },
       error => console.log(error),
       () => {
@@ -119,5 +132,21 @@ export class CategoriesListPage {
 
   public openLinkInSystem(url) {
     window.open(url, '_system');
+  }
+
+  private setNameByLanguage(categoryData) {
+    for (let i = 0; i < categoryData.results.length; i++) {
+      if (this._userPref.getSelectedLanguage().name === 'Francais') {
+        categoryData.results[i].categoryName = categoryData.results[i].categoryNameFr;
+        for (let j = 0; j < categoryData.results[i].subcat.length; j++) {
+          categoryData.results[i].subcat[j].SubCatName = categoryData.results[i].subcat[j].SubCatNameFr;
+        }
+      } else if (this._userPref.getSelectedLanguage().name === 'العربية') {
+        categoryData.results[i].categoryName = categoryData.results[i].categoryNameAr;
+        for (let j = 0; j < categoryData.results[i].subcat.length; j++) {
+          categoryData.results[i].subcat[j].SubCatName = categoryData.results[i].subcat[j].SubCatNameAr;
+        }
+      }
+    }
   }
 }
