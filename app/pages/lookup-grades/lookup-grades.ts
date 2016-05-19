6d0825a63740ad473468a860a429bab3f3ba458c
Jad Salhani <salhanijad@gmail.com>
2016-05-19 14:21:18 +0300

    adds google maps

--- app/pages/lookup-grades/lookup-grades.ts
+++ app/pages/lookup-grades/lookup-grades.ts
@@ -1,5 +1,7 @@
 import {Page, NavController, Loading} from 'ionic-angular';
-import {Http, Headers} from 'angular2/http';
+import {Http} from 'angular2/http';
+
+declare var google;
 
 /*
   Generated class for the LookupGradesPage page.
@@ -13,14 +15,41 @@ import {Http, Headers} from 'angular2/http';
 export class LookupGradesPage {
   public studentNumber: string;
   public studentClass: string;
+  // public loadCenterMap: boolean = false;
+  public map;
   public studentGradeDetail;
 
   constructor(private _nav: NavController, private _http: Http) {
 
   }
 
+  public onPageLoaded() {
+  }
+
   public searchForGrades() {
     if (!this.validate()) {
+      return false;
+    }
+    if (this.studentNumber === "1020") {
+      this.studentGradeDetail = null;
+      let loader = Loading.create({
+        content: "Loading map to Center",
+        duration: 20000
+      });
+
+      this._nav.present(loader);
+
+      this.getCenterLocation("SCH-1003");
+      return false;
+    } else if (this.studentNumber === "1000") {
+      this.studentGradeDetail = null;
+      let loader = Loading.create({
+        content: "Loading map to Center",
+        duration: 20000
+      });
+      this._nav.present(loader);
+
+      this.getCenterLocation("SCH-104");
       return false;
     }
 
@@ -68,5 +97,50 @@ export class LookupGradesPage {
       return false;
     }
     return true;
+  }
+
+  private getCenterLocation(centerId: string) {
+    let location = null;
+    this._http.get(`https://mehe.firebaseio.com/centers/${centerId}.json`)
+      .map(res => res.json())
+      .subscribe((centerInfo) => {
+        location = {
+          latitude: centerInfo.latitude,
+          longitude: centerInfo.longitude,
+          centerName: centerInfo.name
+        };
+      }, (error) => {
+        console.log(error);
+      }, () => {
+        this.loadMap(location.latitude, location.longitude, location.centerName);
+      });
+  }
+
+  private loadMap(latitude: number, longitude: number, centerName: string) {
+
+    let latLng = new google.maps.LatLng(latitude, longitude);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: mapOptions.center
+    });
+
+    let infowindow = new google.maps.InfoWindow({
+      content: centerName
+    });
+
+    marker.addListener('click', function () {
+      infowindow.open(this.map, marker);
+    });
+
   }
 }
