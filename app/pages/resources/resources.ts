6ff40a88435a46ecafd5fd3db665a6408a654798
Jad Salhani <salhanijad@gmail.com>
2016-05-20 19:38:25 +0300

    adds resources page

---
+++ app/pages/resources/resources.ts
@@ -0,0 +1,14 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the ResourcesPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/resources/resources.html',
+})
+export class ResourcesPage {
+  constructor(public nav: NavController) {}
+}
