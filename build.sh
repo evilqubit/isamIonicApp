4d0c380379ee20127247699c53a2b07fe1632998
Jad Salhani <salhanijad@gmail.com>
2016-05-23 15:06:45 +0300

    fixes on build script

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ ionic plugin add cordova-plugin-crosswalk-webview
 ionic build --release android
 
 # copy android crosswalk apks to build folder
-cp platforms/android/build/outputs/apk/android-x86-release.apk ~/Desktop/android-pre5-x86.apk
-cp platforms/android/build/outputs/apk/android-armv7-release.apk ~/Desktop/android-pre5-armv7.apk
+cp platforms/android/build/outputs/apk/android-x86-release.apk bin/android-pre5-x86.apk
+cp platforms/android/build/outputs/apk/android-armv7-release.apk bin/android-pre5-armv7.apk
 
 # rm crosswalk
 ionic plugin rm cordova-plugin-crosswalk-webview
@@ -17,4 +17,4 @@ ionic plugin rm cordova-plugin-crosswalk-webview
 ionic build --release android -- --minSdkVersion=21
 
 # copy android non-crosswalk apks to build folder
-cp platforms/android/build/outputs/apk/android-release.apk ~/Desktop/android-5.apk
+cp platforms/android/build/outputs/apk/android-release.apk bin/android-5.apk
